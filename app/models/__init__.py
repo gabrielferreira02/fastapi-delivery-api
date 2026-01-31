3434f6aa4a91747bb0c84bfe58e74643d97b01d7
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-01-31 19:28:47 -0300

    feat: add init file to generate mirations successfully

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.category import Category
+from app.models.order import Order
+from app.models.order_item import OrderItem
+from app.models.product import Product
+from app.models.user import User
