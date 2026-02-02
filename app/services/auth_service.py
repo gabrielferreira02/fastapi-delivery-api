0eeb1ba105cedad9c6df945edd5f0cd384b528e9
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-02-02 18:56:51 -0300

    feat: add auth service with not implemented methods

---
+++ app/services/auth_service.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+
+class AuthService:
+    def register(session: Session):
+        pass
+
+    def login(session: Session):
+        pass
+
+    def refresh_token(session: Session):
+        pass
+
+    def forgot_password(session: Session):
+        pass
