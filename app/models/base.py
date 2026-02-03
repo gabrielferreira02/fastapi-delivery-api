786c38b8417f581cee118534ed26bf6503429751
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-02-03 15:36:54 -0300

    refactor: change DB_URL import path

--- app/models/base.py
+++ app/models/base.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
-from app.main import DB_URL
+from app.core.vars import DB_URL
 
 db = create_engine(DB_URL)
 Base = declarative_base()
