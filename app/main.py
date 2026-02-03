b98e8546937d5d83864efab0c70d6f0ec09919df
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-02-03 18:36:23 -0300

    chore: register user routes and bcrypt context

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from passlib.context import CryptContext
 
 app = FastAPI()
 
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 from app.api.routes.auth_routes import auth_router
+from app.api.routes.user_routes import user_router
 
 app.include_router(auth_router)
+app.include_router(user_router)
