9264a7050ab2920fbf0b665003ac8a0119eae6be
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-02-05 19:18:13 -0300

    feat: add is_admin attribute to user model and image_url to category model

--- app/models/category.py
+++ app/models/category.py
@@ -8,7 +8,9 @@ class Category(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column("name", String, nullable=False)
     slug = Column("slug", String, nullable=False, unique=True)
+    image_url = Column("image_url", String, nullable=False)
 
-    def __init__(self, name, slug):
+    def __init__(self, name, slug, image_url):
         self.name = name
         self.slug = slug
+        self.image_url = image_url
--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,5 @@
 from app.models.base import Base
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, DateTime, Boolean
 from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
@@ -12,10 +12,12 @@ class User(Base):
     last_name = Column("last_name", String, nullable=False)
     email = Column("email", String, nullable=False, unique=True)
     password = Column("password", String, nullable=False)
+    is_admin = Column("is_admin", Boolean, default=False, nullable=False)
     created_at = Column("created_at", DateTime(timezone=True), server_default=func.now(), nullable=False)
     
-    def __init__(self, first_name, last_name, email, password):
+    def __init__(self, first_name, last_name, email, password, is_admin=False):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = password
+        self.is_admin = is_admin
