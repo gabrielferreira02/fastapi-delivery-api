1bb078b1e0233518dc8c1f8883c46cb6cd821cae
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-02-05 19:20:15 -0300

    chore: register category routes and mount images upload directory

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from passlib.context import CryptContext
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/images", StaticFiles(directory="app/uploads/images"), name="images")
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 from app.api.routes.auth_routes import auth_router
 from app.api.routes.user_routes import user_router
+from app.api.routes.category_routes import category_router
 
 app.include_router(auth_router)
 app.include_router(user_router)
+app.include_router(category_router)
