63ed7ae26b3ab66150e4e4017b246786a5b21de4
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-02-09 17:30:33 -0300

    chore: register products routes

--- app/main.py
+++ app/main.py
@@ -11,7 +11,9 @@ bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 from app.api.routes.auth_routes import auth_router
 from app.api.routes.user_routes import user_router
 from app.api.routes.category_routes import category_router
+from app.api.routes.product_routes import product_router
 
 app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(category_router)
+app.include_router(product_router)
