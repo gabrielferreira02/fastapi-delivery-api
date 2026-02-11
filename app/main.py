7f98a7fc808be1357c17d97154242dca8693e255
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-02-11 16:44:38 -0300

    chore: register order routes

--- app/main.py
+++ app/main.py
@@ -12,8 +12,10 @@ from app.api.routes.auth_routes import auth_router
 from app.api.routes.user_routes import user_router
 from app.api.routes.category_routes import category_router
 from app.api.routes.product_routes import product_router
+from app.api.routes.order_routes import order_router
 
 app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(category_router)
 app.include_router(product_router)
+app.include_router(order_router)
