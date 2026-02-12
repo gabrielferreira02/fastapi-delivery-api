556f5d3852be1aa84a58598bcddc5dfa7434ed5b
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-02-12 19:54:16 -0300

    feat: add verify token function

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,5 +1,11 @@
 from sqlalchemy.orm import Session, sessionmaker
 from app.models.base import db
+from app.models.user import User
+from fastapi import Depends, HTTPException
+from jose import jwt, JWTError
+from app.core.vars import SECRET_KEY, ALGORITHM
+from app.main import oauth2_schema
+from uuid import UUID
 
 def get_session():
     try:
@@ -8,4 +14,16 @@ def get_session():
         yield session
     finally:
         session.close()
+
+def verify_token(token: str = Depends(oauth2_schema), session: Session = Depends(get_session)):
+    try:
+        dic_info = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id = UUID(dic_info.get("sub"))
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Acesso negado")
+    
+    user = session.query(User).filter(User.id==id).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="Acesso inválido")
+    return user
         
