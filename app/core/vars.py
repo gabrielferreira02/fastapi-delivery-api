41b98122cddb8170854ffc25779110f7cab55a38
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-02-12 19:55:14 -0300

    core: add authentication variables

--- app/core/vars.py
+++ app/core/vars.py
@@ -5,4 +5,6 @@ load_dotenv()
 
 DB_URL = os.getenv("DB_URL")
 UPLOAD_DIR = os.getenv("UPLOAD_DIR")
-
+JWT_EXPIRATION_TIME = os.getenv("JWT_EXPIRATION_TIME")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
